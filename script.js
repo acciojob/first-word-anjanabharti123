d6a161f19739e1e54fce3af8da8e7026f36fe4f0
Anjana Bharti <143540468+anjanabharti123@users.noreply.github.com>
2025-01-03 16:15:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
   // your code here
+	const trimmedStr = s.trim();
+  const firstSpaceIndex = trimmedStr.indexOf(' ');
+
+  // If no space is found, return the entire string
+  if (firstSpaceIndex === -1) {
+    return trimmedStr;
+  }
+
+  // Return the first word
+  return trimmedStr.slice(0, firstSpaceIndex);
 }
 
 // Do not change the code below
